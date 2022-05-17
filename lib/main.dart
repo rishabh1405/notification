6026af9df890af73949c8a1664d6ee8a1bb9bb26
rishabh1405 <singh.rishabh30@gmail.com>
2022-05-17 17:35:04 +0530

    data 1 text

--- lib/main.dart
+++ lib/main.dart
@@ -110,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text("data"),
+            Text("data 1"),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
